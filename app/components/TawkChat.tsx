c6182c0a069a45be13f924cc1b735d78395f41dc
sonerkayan17-commits <sonerkayan17@gmail.com>
2026-08-12 11:31:59 +0300

    Use Sky Bozum Tawk widget

--- app/components/TawkChat.tsx
+++ app/components/TawkChat.tsx
@@ -3,7 +3,7 @@
 import { useEffect } from 'react';
 
 const TAWK_PROPERTY_ID = '69fa97b6b2c8791c30a70ea8';
-const TAWK_WIDGET_ID = '1jnte1897';
+const TAWK_WIDGET_ID = '1jvqhf0gc';
 const SCRIPT_ID = 'sky-bozum-tawk-chat';
 
 /** Loads the official Sky Bozum support widget once for the whole site. */
